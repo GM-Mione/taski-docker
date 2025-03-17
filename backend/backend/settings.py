11a09aaa7f9ff006fbc25e6d7a8c50032d1dac1a
Galina Melihova <tulupane@yandex.ru>
2025-03-17 23:16:49 +0700

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['89.169.175.71', 'localhost', 'yphost.zapto.org']
+ALLOWED_HOSTS = ['89.169.175.71', '127.0.0.1', 'localhost', 'yphost.zapto.org']
 
 # Application definition
 
